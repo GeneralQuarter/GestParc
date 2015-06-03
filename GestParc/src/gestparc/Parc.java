a9e869d6d6091d4a18ab052ea6061c07fd2db761
vpivet <vpivet@AGP14.univ-lr.fr>
2015-06-03 17:11:47 +0200

    Implémentation de la classe Parc.

--- GestParc/src/gestparc/Parc.java
+++ GestParc/src/gestparc/Parc.java
@@ -6,10 +6,33 @@
 
 package gestparc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author qgangler
  */
 public class Parc {
+    private List<Vehicule> vehicules;
+
+    public Parc() {
+        this.vehicules = new ArrayList<>();
+    }
     
+    public void ajouterVehicule(Vehicule vehicule){
+        if(!vehicules.contains(vehicule)){
+            vehicules.add(vehicule);
+        }else{
+            System.out.println("Le vehicule existe déjà dans le parc.");
+        }
+    }
+    
+    public void retirerVehicule(Vehicule vehicule){
+        if(vehicules.contains(vehicule)){
+            vehicules.remove(vehicule);
+        }else{
+            System.out.println("Le vehicule n'existe pas dans le parc.");
+        }
+    }
 }
