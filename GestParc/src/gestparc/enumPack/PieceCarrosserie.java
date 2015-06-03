9074f62f7822e84171d2619e29351ba75e8c7ac0
vpivet <vpivet@AGP14.univ-lr.fr>
2015-06-03 16:52:23 +0200

    Implémentation des autres Enum.

---
+++ GestParc/src/gestparc/enumPack/PieceCarrosserie.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gestparc.enumPack;
+
+/**
+ *
+ * @author vpivet
+ */
+public enum PieceCarrosserie {
+    PORTIERE_LATERALE_AVANT_GAUCHE,
+    PORTIERE_LATERALE_AVANT_DROITE,
+    PORTIERE_LATERALE_ARRIERE_GAUCHE,
+    PORTIERE_LATERALE_ARRIERE_DROITE,
+    PORTE_ARRIERE_GAUCHE,
+    PORTE_ARRIERE_DROITE,
+    COFFRE,
+    CAPOT,
+    PARE_CHOC_AVANT,
+    PARE_CHOC_ARRIERE,
+    RETRO_COQUILLE_GAUCHE,
+    RETRO_COQUILLE_DROIT,
+    CALANDRE,
+    BAGUETTE,
+    AILE_AVANT_GAUCHE,
+    AILE_AVANT_DROITE,
+    AILE_ARRIERE_GAUCHE,
+    AILE_ARRIERE_DROITE;
+}
---
+++ GestParc/src/gestparc/enumPack/PieceMecanique.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gestparc.enumPack;
+
+/**
+ *
+ * @author vpivet
+ */
+public enum PieceMecanique {
+    ALTERNATEUR, DEMARREUR, VENTILATEUR,
+    EMBRAYAGE, BATTERIE, BOBINE_ALLUMAGE,
+    BOUGIE_ALLUMAGE, DISQUE_FREIN, PLAQUETTE_FREIN,
+    ETRIER_FREIN, TAMBOUR, AMORTISSEUR,
+    MOTEUR_ENTIER, AUTRE;
+}
